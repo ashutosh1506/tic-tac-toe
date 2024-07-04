fb302f65a1ed4894190f49038de8ce1bfcad692c
ashutosh1506 <guptaashutosh502@gmail.com>
2024-07-04 15:02:27 +0530

    Initial Commit

---
+++ src/components/Board.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Tile from "./Tile";
+import Strike from "./Strike";
+
+const Board = () => {
+  return (
+    <div>
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Strike />
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/components/Tile.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tile = () => {
+  return <div>X</div>;
+};
+
+export default Tile;
